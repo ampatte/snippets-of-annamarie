eb9ee0bfa70c12534b9112131db9bfa114f79a7d
ampatte <ampatterson1@hotmail.com>
2023-03-25 19:46:30 -0400

    set up components/Pages with base code

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/Footer.css';
+
+function Footer() {
+    return (
+    <footer className='footer'>
+        <ul>
+            <li>GitHub</li>
+            <li>LinkedIn</li>
+            <li>Stack Overflow</li>
+        </ul>
+    </footer>
+    );
+}
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import '../styles/Header.css';
+
+function Header({ currentPage, handlePageChange }) {
+    return (
+      <nav>
+        <ul className="nav nav-tabs">
+          <li className="nav-item">
+            <a
+              href="#home"
+              onClick={() => handlePageChange('Home')}
+              className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+            >
+              Anna Marie Patterson
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="#about"
+              onClick={() => handlePageChange('About')}
+              className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+            >
+              About Me
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="#portfolio"
+              onClick={() => handlePageChange('Portfolio')}
+              className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+            >
+              Portfolio
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="#contact"
+              onClick={() => handlePageChange('Contact')}
+              className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+            >
+              Contact
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="#resume"
+              onClick={() => handlePageChange('Resume')}
+              className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+            >
+              Resume
+            </a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+
+  export default Header;
