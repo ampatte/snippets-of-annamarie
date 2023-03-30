acfbc2fb616340cd69a4674543ba427680bcc0df
ampatte <ampatterson1@hotmail.com>
2023-03-30 07:41:30 -0400

    Remove script tags from html, added to serviceWorker

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -14,7 +14,7 @@ export function register(config) {
         //this SW won't work if PUBLIC_URL is on a different origin from what
         //our page is served on. This might happen if a CDN is used to serve
         //assets; see https:github.com/facebook/create-react-app/issues/2374
-            return
+            return;
         }
     
         window.addEventListener('load', () => {
