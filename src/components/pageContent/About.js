bf05ec35e5f5df886c22ce35090ecec004ac37af
ampatte <ampatterson1@hotmail.com>
2023-06-21 18:51:41 -0400

    Updated About Me paragraph

--- src/components/pageContent/About.js
+++ src/components/pageContent/About.js
@@ -52,9 +52,8 @@ export default function About() {
             padding:'2%',
             marginBottom:'5%'}}
         >
-          My name is Anna Marie and I am a Full Stack Web Developer! I am a former preschool and art teacher turned techie! Embarking on this career change has been such an exciting journey! For me, the most interesting part about web development is seeing the "how" of what is displayed on the screen and being a part of it's creation! In the past I've often said I should've been an engineer because I'm so interested in learning how things work. I am also very artsy! Creating something new, whether painting on canvas or doing a DIY project will keep me busy for hours. Now that love has been translated into a love for web development! I come from a large family, where two of my four brothers are successful software developers. Becoming the third in the family is a goal of mine that I am ready to acheive!
-        </Paper>
-      
+          My name is Anna Marie, and I am a Full Stack Web Developer. I am a former preschool and art teacher turned techie. Embarking on this career change has been such an exciting journey. The most facinating part about web development is seeing the "how" behind what is displayed on the screen. I am especially interested in the style element of development. Designing and Creating something new, has always been a point of motivation. Now that has been translated into a love for web development. I come from a large family, where two of my four brothers are successful software developers. Becoming the third in the family is a goal of mine that I'm prepared to acheive.
+          </Paper>
       </Stack>
     </div>
   );
