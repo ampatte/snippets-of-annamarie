78099b68acea9c4816837f4b4385c00b76c1cd48
ampatte <ampatterson1@hotmail.com>
2025-03-11 20:28:11 -0400

    styling

--- src/components/pageContent/Portfolio.js
+++ src/components/pageContent/Portfolio.js
@@ -1,24 +1,38 @@
 import React from 'react';
 import Project from './Project';
-
+import { Paper, Card, Stack } from '@mui/material';
 import pexelsMigueláPadriñán from '../../assets/backgrounds/pexelsMigueláPadriñán.jpg';
 
 function Portfolio() {
 
   return (
-    <div
-      className='portfolio'
-      style={
-        { backgroundImage:`url(${ pexelsMigueláPadriñán })`,
-          backgroundRepeat:'no-repeat',
-          backgroundSize:'cover',
-          height:'100vh'
-        }}>
-    
-      <h1 style={{ fontFamily: 'Instrument Serif', fontWeight:'bold'}}>Portfolio</h1>
-      
-      <Project/>
-
+    <div>
+      <Stack>
+        <Paper 
+            className='cards'
+            elevation={8} 
+            style={{
+              textAlign:'center',
+              fontSize:'xx-large',
+              fontFamily:'WindSong',
+              border:'thick double',
+              borderColor:'4c4c4d'}}
+          >Portfolio
+        </Paper>
+        
+        <Card         
+          className='cards'
+          elevation={8} 
+          style={{
+            backgroundImage:`url(${pexelsMigueláPadriñán})`,
+            backgroundRepeat:'no-repeat',
+            backgroundSize:'cover',            
+            marginBottom:'2%',
+            padding:'1%'}} 
+          >
+          <Project/>
+        </Card>
+      </Stack>
     </div>
   );
 }
